de5e84cbbd68df9d262cd0a6d4e53e254e4c1ee5
Br1gery <pipecppc7@gmail.com>
2024-03-24 23:01:46 +0500

    build_index_page

--- pages/build/index.vue
+++ pages/build/index.vue
@@ -1,11 +1,6 @@
 <template>
     <div class="container flex flex-row flex-wrap justify-between gap-y-10">
-        <buildCard></buildCard>
-        <buildCard></buildCard>
-        <buildCard></buildCard>
-        <buildCard></buildCard>
-        <buildCard></buildCard>
-
+        <buildCard v-for="i in 7" :league="'Первое бытие'" :build_class="'Антихрист'" :build_lvl="100" :build_name="'xdders'"></buildCard>
     </div>
 </template>
 
