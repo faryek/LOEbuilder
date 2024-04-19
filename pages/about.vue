61611effc3d49bca1a1719071119f6f0914e97ff
Br1gery <pipecppc7@gmail.com>
2024-04-19 21:04:45 +0500

    builds_page_icons

--- pages/about.vue
+++ pages/about.vue
@@ -4,8 +4,6 @@
             планирования билдов в нашей игре Last of Exiles. Поддержка функционала будет осуществляться в соотетствии с
             порядком обновлений LoE.</p>
         <div class="flex flex-col gap-5">
-            <img src="https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg" alt="">
-            <p class="text-white text-5xl w-100 font">Слава НТИ!</p>
         </div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,34 @@
             <option value="Мать" class="selector">Маппинг</option>
         </select>
     </div>
+    <div class="container character_selector">
+        <div class="wrapper flex flex-row relative justify-between">
+            <div class="icon char_icon1 flex flex-col-reverse">
+                <p>Антихрист</p><img src="/assets/img/classes/Antihrist/Small.png" alt="">
+            </div>
+            <div class="icon char_icon2 flex flex-col-reverse">
+                <p>Богатырь</p><img src="/assets/img/classes/Bogatir/Small.png" alt="">
+            </div>
+            <div class="icon char_icon3 flex flex-col-reverse">
+                <p>Боголюб</p><img src="/assets/img/classes/Bogolub/Small.png" alt="">
+            </div>
+            <div class="icon char_icon4 flex flex-col-reverse">
+                <p>Застрельщица</p><img src="/assets/img/classes/Zastrel/Small.png" alt="">
+            </div>
+            <div class="icon char_icon5 flex flex-col-reverse">
+                <p>Налётчик</p><img src="/assets/img/classes/Medvesh/Small.png" alt="">
+            </div>
+            <div class="icon char_icon6 flex flex-col-reverse">
+                <p>Богохульник</p><img src="/assets/img/classes/Pahan/Small.png" alt="">
+            </div>
+            <div class="icon char_icon7 flex flex-col-reverse">
+                <p>Скоморох</p><img src="/assets/img/classes/Skomoroh/Small.png" alt="">
+            </div>
+            <div class="icon char_icon8 flex flex-col-reverse">
+                <p>Язычница</p><img src="/assets/img/classes/Yazich/Small.png" alt="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -22,8 +50,29 @@
 </script>
 
 <style>
-    .selector{
-        border: 3px solid rgba(0,0,0,0);
-        border-radius: 25px;
-    }
+.selector {
+    border: 3px solid rgba(0, 0, 0, 0);
+    border-radius: 25px;
+}
+
+.icon {
+    height: 110px;
+    width: 110px;
+    border-radius: 15px;
+}
+
+.icon img {
+    height: 110px;
+    width: 110px;
+    object-fit: cover;
+    border-radius: 15px;
+}
+
+.icon p {
+    position: absolute;
+    width: 110px;
+    background-color: white;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+}
 </style>
