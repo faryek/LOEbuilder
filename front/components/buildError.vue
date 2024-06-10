5acbb961b7349ff35d8fc9abaa5b71a096efa4da
faryek <ermak1871@gmail.com>
2024-06-10 18:18:50 +0500

    Create buildError.vue

---
+++ front/components/buildError.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="error-form container border-shine py-10 px-10">
+        <p class="font text-4xl">404, Билд не найден...</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.error-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5vh;
+    width: 30%;
+    background-color: black;
+    border: 3px solid;
+    border-radius: 25px;
+    border-color: #9b7e41 !important;
+    box-shadow: 0 0 20px 0 #9b7e41;
+}
+</style>
